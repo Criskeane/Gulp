14955c9ec3bb0efefdb7226e17ab78e086db6df3
Criskeane <35363215+Criskeane@users.noreply.github.com>
2018-05-21 18:04:23 +0700

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,12 @@
 var gulp = require('gulp'),
     bourbon = require('bourbon').includePaths,
     neat = require('bourbon-neat').includePaths,
-    newer = require('gulp-newer'),
-    imagemin = require('gulp-imagemin'),
-    htmlclean = require('gulp-htmlclean'),
-    concat = require('gulp-concat'),
     deponder = require('gulp-deporder'),
-    stripdebug = require('gulp-strip-debug'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    scsslint = require('gulp-scss-lint'),
-    postcss = require("gulp-postcss"),
     assets = require("postcss-assets"),
     autoprefixer = require('autoprefixer'),
     mqpacker = require('css-mqpacker'),
-    cssnano = require('gulp-cssnano'),
-    cache = require('gulp-cache'),
     browserSync = require('browser-sync'),
     del = require('del'),
-    sourcemap = require('gulp-sourcemaps'),
-    changed = require('gulp-changed'),
-    inject = require('gulp-inject'),
     runSequence = require('run-sequence'),
     bowerFile = require('main-bower-files'),
     es = require('event-stream'),
@@ -36,68 +21,72 @@ var gulp = require('gulp'),
         build: 'build/'
     };
 
-// var paths = {
-//     src: 'app/**/*',
-//     srcHTML: 'app/**/*.html',
-//     srcSCSS: 'app/**/*.scss',
-//     srcJS:  'app/**/*.js',
-
-//     tmp: 'tmp',
-//     tmpIndex: 'tmp/index.html',
-//     tmpCSS: 'tmp/**/*.css',
-//     tmpJS: 'tmp/**/*.js',
-  
-//     builddev: 'build',
-//     builddevHTML: 'build/index.html',
-//     builddevCSS: 'build/**/*.css',
-//     builddevJS: 'build/**/*.js'
-// }
-
+var plugins = require('gulp-load-plugins')();
 
 //CLEAN BUILD
 gulp.task('clean', function(){
     return del(['build'])
 });
 
-//INJECT FILE
+//INJECT FILE BUILD
 gulp.task('inject', function(){
     var source = gulp.src([
         folder.build + '**/*.css', folder.build + '**/*.js'
     ])
 
     gulp.src(folder.build + '**/*.html')
-    .pipe(inject(
+    .pipe(plugins.inject(
         source, {ignorePath: 'build', addRootSlash: false }
     ))
     .pipe(gulp.dest('./build'));
 });
 
-//COPY FILE 
-gulp.task('copy', function(){
-    var pathjs = folder.src + '/js/jquery.min.js';
-    //var pathfont = folder.src + 'fonts/**/*';
+//INJECT FILE DEV
+gulp.task('inject:dev', function(){
+    var source = gulp.src([
+        folder.build + '**/*.css', folder.build + '**/*.js'
+    ])
+
+    gulp.src(folder.build + '**/*.html')
+    .pipe(plugins.inject(
+        source, {ignorePath: 'app', addRootSlash: false }
+    ))
+    .pipe(gulp.dest('./app'));
+});
+
+//COPY FILE FONT
+gulp.task('copyfont', function(){
+    var pathfont = folder.src + 'fonts/**/*';
     var dests = 'build';
 
-    return gulp.src(pathjs)
-    .pipe(changed(dests))
-    .pipe(gulp.dest(dests + '/js'));
+    return gulp.src(pathfont)
+    .pipe(plugins.changed(dests))
+    .pipe(gulp.dest(dests + '/font'));
 });
 
 //SPIN UP A SERVER
-gulp.task('browserSync', function(){
-    browserSync({
-        server:{
-            baseDir: 'build'
-        }
-    })
+gulp.task('serve', function(){
+    browserSync.init({
+        // proxy: "http://localhost:3000/",
+        // server:{
+        //     baseDir: 'build',
+        //     temp: 'tmp'
+        //     //directory: true
+        // }
+        server: { baseDir: 'build' },
+        proxy: null,
+        files: [ 'build' ],
+        reloadDelay: 50
+    });
+    //gulp.watch('app/**/*.*').on('change', browserSync.reload);
 });
 
 //IMAGES processing
 gulp.task('imagemin', function(){
     var path = folder.build;
     return gulp.src(folder.src + '**/images/**/*.{jpg,jpeg,gif,svg,png}')
-    .pipe(newer(path))
-    .pipe(cache(imagemin({ 
+    .pipe(plugins.newer(path))
+    .pipe(plugins.cache(plugins.imagemin({ 
         optimizationLevel: 5,
         interlaced: true
     })))
@@ -109,10 +98,10 @@ gulp.task('html', ['imagemin'], function(){
     var path = folder.build,
         page = gulp.src( folder.src + '**/**/*.html')
         //.pipe(htmlclean())
-        .pipe(newer(path));
+        .pipe(plugins.newer(path));
 
     if (!devBuild){
-        page = page.pipe(htmlclean());
+        page = page.pipe(plugins.htmlclean());
     }
 
     return page.pipe(gulp.dest(path));
@@ -120,15 +109,22 @@ gulp.task('html', ['imagemin'], function(){
 
 // JavaScript processing
 gulp.task('js', function(){
+    var pathjs = folder.src + '/js/jquery.min.js';
+    var dests = 'build';
+
+    gulp.src(pathjs)
+        .pipe(plugins.changed(dests))
+        .pipe(gulp.dest(dests + '/js'));
+
     var jsbuild = gulp.src(
         folder.src + ['js/**/!(jquery.min)*.js']
     )
     .pipe(deponder())
     //.pipe(uglify())
-    .pipe(concat('main.js'));
+    .pipe(plugins.concat('main.js'));
 
     if(!devBuild){
-        jsbuild = jsbuild.pipe(stripdebug()).pipe(uglify());
+        jsbuild = jsbuild.pipe(plugins.stripdebug()).pipe(plugins.uglify());
     }
 
     return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
@@ -136,6 +132,7 @@ gulp.task('js', function(){
 
 // SCSS processing
 gulp.task('scss', ['imagemin'], function(){
+    var csssrc = [folder.src + 'scss/**/*.scss']
     var postCSSopt = [
         assets({ loadPaths: ['images/'] }),
         autoprefixer ({ browsers: ['last 3 versions', "safari 5", "ie 8", "ie 9", '> 2%'] }),
@@ -143,32 +140,35 @@ gulp.task('scss', ['imagemin'], function(){
     ];
 
     if(!devBuild){
-        postCSSopt.push(cssnano);
+        postCSSopt.push(cssnano());
     }
 
-    return gulp.src(folder.src + 'scss/**/*.scss')
-    .pipe(sass({
-        includePaths: require('node-normalize-scss').includePaths,
-        errLogToConsole: true,
+    return gulp.src([folder.src + 'scss/**/*.scss', '!**/_*.scss'])
+    .pipe(plugins.sass({
         outputStyle: 'compact', 
+        precision: 3
         //outputStyle: 'compressed',
-        imagePaths: 'images/',
-        precision: 3,
-        errLogToConsole: true
     }))
-    //.pipe(cssnano())
-    .pipe(postcss(postCSSopt))
+    //.pipe(plugins.cssnano())
+    .pipe(plugins.plumber({
+        errorHandler : function (error){
+            console.log(error.toSring());
+            this.emit('end');
+        }
+    }))
+    .pipe(plugins.postcss(postCSSopt))
+    //.pipe(concat('style.min.css'))
     .pipe(gulp.dest(folder.build + 'css/'))
     .pipe(browserSync.reload({
         stream: true
     }))
 });
 
-gulp.task('run', function(){
-    runSequence('clean', 'html', 'scss', 'js', 'copy');
+gulp.task('run', ['clean'], function(){
+    runSequence('js', 'scss', 'html', 'copyfont', 'inject');
 });
 
-gulp.task('watch',['browserSync', 'html', 'scss'], function(){
+gulp.task('watch',['serve', 'html', 'scss'], function(){
     //image changes
     gulp.watch(folder.src + '**/images/**/*.{jpg,jpeg,gif,svg,png}', ['imagemin']);
 
@@ -183,5 +183,5 @@ gulp.task('watch',['browserSync', 'html', 'scss'], function(){
 });
 
 gulp.task('default', function(){
-    runSequence('run', 'watch', 'inject');
+    runSequence('run', 'watch');
 });
